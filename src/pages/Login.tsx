866b34c9a14602197485af40744aef9b898b22a3
armenasatryan9603 <armenasatryan.personal@gmail.com>
2026-01-29 18:09:58 +0400

    small fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 // LOGIN DISABLED - Login functionality commented out
-import React, { useState } from 'react';
+import React from 'react';
 // import { useNavigate } from 'react-router-dom';
 // import { useAuth } from '../contexts/AuthContext';
 
